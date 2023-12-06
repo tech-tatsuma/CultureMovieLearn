9f1b7950bc2cd937f4ce7b6f398cc19619dc8c2b
古家 龍磨 <furuya.tatsuma.7sz@eng.nipponsteel.com>
2023-12-06 13:43:29 +0900

    edit readme

--- README.md
+++ README.md
@@ -1,49 +1,38 @@
-# SlowFastNetworksを使った動画分類
-## 学習環境の構築
-### 学習環境の立ち上げ
-```
-sudo docker build -t culturevideolearn .
-```
-### 学習環境の開始
-```
-sudo docker run --rm -it --gpus all -v $PWD:/culturevideolearn culturevideolearn:latest
-```
-
-## Requirements
-
-- Python 3
-- moviepy: 動画の読み込みと分割に使用
-- pandas: データ操作とCSVファイルの作成に使用
-- argparse: コマンドライン引数の処理に使用
-## 実行方法
-### データの作成
-```
-python create_data.py --video_path [動画ファイルへのパス] --timeline_csv_path [タイムラインCSVファイルへのパス] --output_folder [出力ディレクトリへのパス]
-```
-このスクリプトは動画を１秒間隔のセグメントに分割し、それらにラベルをつけてcsvファイルに出力するために使用される。
-
-- --video_path: 処理する動画ファイルへのパス
-- --timeline_csv_path: 動画の各セグメントに対応するラベルが記述されたCSVファイルへのパス
-- --output_folder: 分割された動画ファイルとラベル付けされたCSVを保存するディレクトリへのパス
-### データの学習
-```
-python train.py --data [csvファイルパス] --epochs [学習回数] --patience [早期終了パラメータ] --batch [バッチサイズ] --lr [学習率] --seed [ランダムシード] --lr_search [学習率探索]
-```
-
-- --data: create_data.pyの実行により作成されたcsvファイルのパスを指定する
-- --patience: validation lossの値がpatience回連続で下がらなかった場合にプログラムは早期終了する
-- --lr_search: 最適な学習率が分からない場合はこのパラメータを'true'にする
-### テスト
-```
-python test.py --csv_file [テストに利用するcsvのパス] --model_path [モデルのパス] --output_csv [出力するcsvファイルのパス] 
-```
-
-### Note
-- タイムラインCSVファイルは、各行に時間,ラベルの形式で記述されている必要がある。
-- 出力されるCSVファイルには、各セグメントのファイルパスと対応するラベルが含まれる。
+# ビデオ予測API
+１秒間のビデオファイルをリクエストとして受け取り、学習済みモデルを使用して予測を行うFastAPIベースのAPIを実装したプロジェクト
 ## Model
 ### CustomSlowFastネットワーク
 このネットワークは、動画分類のためのSlowFastネットワークアーキテクチャを使用したカスタムニューラルネットワーク。SlowFastモデルは動画内の早い動きと遅い動きの両方を捉えることができるため、動画内の物体の動きに敏感なネットワークとなっている。
+## APIの立ち上げ
+```
+python router.py
+```
+デフォルトでは、APIはhttp://127.0.0.1:8000 で立ち上がるようになっているが、独自の環境に合わせて利用する。
+## リクエスト方法
+predict-videoエンドポイント
+
+- メソッド：POST
+- 説明：このエンドポイントは、ビデオファイルを受け取り、ビデオに対する予測を行う
+- リクエスト形式：フォームデータでビデオファイルをアップロードする
+### リクエスト例
+```
+curl -X 'POST' \
+  'http://127.0.0.1:8000/predict-video' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: multipart/form-data' \
+  -F 'file=@path_to_video_file'
+```
+### レスポンス形状
+- current: 現在のフレームの予測結果
+- next: 次のフレームの予測結果
+```
+{
+  "current": true,
+  "next": false
+}
+```
+- true：給餌
+- false: 給餌停止
 
 ### 参考
 - https://arxiv.org/abs/1812.03982
---
+++ rooter.py
@@ -0,0 +1,68 @@
+import torch
+
+from pytorchvideo.transforms import UniformTemporalSubsample
+from torchvision.transforms import Compose, Lambda, Normalize, Resize
+from torchvision.io import read_video
+
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+from typing import Dict
+import shutil
+import os
+
+from model import CustomSlowFast
+
+def load_model(model_path, device):
+    model = CustomSlowFast(device=device, num_classes=1).to(device)
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    return model
+
+app = FastAPI()
+model_path = "path_to_model_file"
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = load_model(model_path, device)
+
+def normalize_video(video, mean, std):
+    normalized_video = []
+    for frame in video.permute(1, 0, 2, 3):
+        normalized_frame = Normalize(mean, std)(frame)
+        normalized_video.append(normalized_frame)
+    return torch.stack(normalized_video, dim=1)
+
+def video2tensor(video_path):
+    transform = Compose([
+        UniformTemporalSubsample(25),
+        Lambda(lambda x: x / 255.0),
+        Lambda(lambda x: normalize_video(x, mean=[0.45, 0.45, 0.45], std=[0.225, 0.225, 0.225])),
+        Resize((256, 256)),
+    ])
+
+    video, _, _ = read_video(video_path, start_pts=0, end_pts=None, pts_unit='sec')
+    video = video.permute(3, 0, 1, 2)
+    video = transform(video)
+
+    return video
+
+@app.post("/predict-video")
+async def predict_video(file: UploadFile = File(...)):
+    temp_file_path = f"temp_{file.filename}"
+    
+    with open(temp_file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    video_tensor = video2tensor(temp_file_path)
+
+    with torch.no_grad():
+        current_output, next_output = model(video_tensor.unsqueeze(0))
+        current_output = torch.sigmoid(current_output)
+        next_output = torch.sigmoid(next_output)
+        predicted_current = (current_output >=0.5).squeeze()
+        predicted_next = (next_output >=0.5).squeeze()
+
+    os.remove(temp_file_path)
+
+    return {"current": predicted_current.item(), "next": predicted_next.item()}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
