33977d7f8cf09c876142538e02975e15e2b0b81e
tech-tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2023-12-14 12:48:57 +0900

    tweak

--- rooter.py
+++ rooter.py
@@ -19,6 +19,8 @@ def load_model(model_path, device):
     return model
 
 app = FastAPI()
+
+# replace this with your model path
 model_path = "path_to_model_file"
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = load_model(model_path, device)
@@ -30,12 +32,37 @@ def normalize_video(video, mean, std):
         normalized_video.append(normalized_frame)
     return torch.stack(normalized_video, dim=1)
 
+class VideoColorJitter:
+    def __init__(self, brightness=0, contrast=0, saturation=0, hue=0):
+        self.brightness = brightness
+        self.contrast = contrast
+        self.saturation = saturation
+        self.hue = hue
+
+    def __call__(self, video):
+        video = video.permute(1, 0, 2, 3)
+        brightness_factor = random.uniform(max(0, 1 - self.brightness), 1 + self.brightness)
+        contrast_factor = random.uniform(max(0, 1 - self.contrast), 1 + self.contrast)
+        saturation_factor = random.uniform(max(0, 1 - self.saturation), 1 + self.saturation)
+        hue_factor = random.uniform(-self.hue, self.hue)
+
+        transformed_video = []
+        for frame in video:
+            frame = FT.adjust_brightness(frame, brightness_factor)
+            frame = FT.adjust_contrast(frame, contrast_factor)
+            frame = FT.adjust_saturation(frame, saturation_factor)
+            frame = FT.adjust_hue(frame, hue_factor)
+            transformed_video.append(frame)
+
+        return torch.stack(transformed_video).permute(1, 0, 2, 3)
+
 def video2tensor(video_path):
     transform = Compose([
         UniformTemporalSubsample(25),
         Lambda(lambda x: x / 255.0),
         Lambda(lambda x: normalize_video(x, mean=[0.45, 0.45, 0.45], std=[0.225, 0.225, 0.225])),
         Resize((256, 256)),
+        VideoColorJitter(brightness=0.3)
     ])
 
     video, _, _ = read_video(video_path, start_pts=0, end_pts=None, pts_unit='sec')
@@ -56,8 +83,8 @@ async def predict_video(file: UploadFile = File(...)):
     with torch.no_grad():
         current_output, next_output = model(video_tensor.unsqueeze(0))
         
-        _, predicted_current = torch.max(current_outputs, 1)
-        _, predicted_next = torch.max(next_outputs, 1)
+        predicted_current = torch.sigmoid(current_outputs)
+        predicted_next = torch.sigmoid(next_outputs)
 
     os.remove(temp_file_path)
 
