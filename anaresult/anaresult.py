8df3002205391f5c1fcfb7da0f7045b55f416b15
古家 龍磨 <furuya.tatsuma.7sz@eng.nipponsteel.com>
2023-12-19 09:48:35 +0900

    addana

---
+++ anaresult/anaresult.py
@@ -0,0 +1,90 @@
+import cv2
+import csv
+import os
+
+def write_values_on_video(video_path, value, status, output_path):
+    # 動画を読み込む
+    cap = cv2.VideoCapture(video_path)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    # 書き込み用のビデオライターを設定
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # テキストを書き込む
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame, f'Value: {value}', (width - 200, 30), font, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+        cv2.putText(frame, f'Status: {status}', (width - 200, 60), font, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+
+        # 書き込んだフレームを出力
+        out.write(frame)
+
+    # リソースを解放
+    cap.release()
+    out.release()
+
+def concatenate_videos(video_paths, output_path):
+    # 最初の動画を読み込んで、フレームサイズとフレームレートを取得
+    cap = cv2.VideoCapture(video_paths[0])
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    # 書き込み用のビデオライターを設定
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+
+    for video_path in video_paths:
+        cap = cv2.VideoCapture(video_path)
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            # フレームを出力動画に書き込む
+            out.write(frame)
+
+        cap.release()
+
+    # 出力ファイルを閉じる
+    out.release()
+
+def process_csv_and_write_text(csv_path, final_output_path):
+
+    csv_dir = os.path.dirname(csv_path)
+
+    with open(csv_path, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # 最初の行（ヘッダー）をスキップ
+
+        outputs = []
+
+        for index, row in enumerate(reader):
+            relative_video_path = row[0]
+            video_path = os.path.join(csv_dir, relative_video_path)
+            prediction = float(row[2])
+            status = '給餌中' if prediction >= 0.2 else '給餌停止'
+            output_path = f'output_{index}.mp4'  # 出力ファイル名を生成
+
+            write_values_on_video(video_path, prediction, status, output_path)
+            outputs.append(output_path)
+
+        concatenate_videos(outputs, final_output_path)
+
+        # 一時動画ファイルを削除
+        for output in outputs:
+            os.remove(output)
+
+if __name__ == '__main__':
+    csv_path = 'result.csv'
+    final_output_path = 'output.mp4'
+    process_csv_and_write_text(csv_path, final_output_path)
+
